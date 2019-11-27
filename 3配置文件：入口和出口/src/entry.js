8a3a8c718397edd17e384ceaa0bd3cad6bb14d1f
renzhiwei <729079155@qq.com>
2019-11-27 14:43:08 +0800

    首次提交

---
+++ 3配置文件：入口和出口/src/entry.js
@@ -0,0 +1,2 @@
+document.getElementById('title').innerHTML='Hello Webpack';
+alert("成功了！")
---
+++ 5模块：CSS文件打包/webpack.config.js
@@ -0,0 +1,42 @@
+const path=require("path");
+
+module.exports={
+    //入口文件配置,可以是单一入口，也可以是多入口。
+    entry:{
+        entry:"./src/entry.js",
+        entry2:"./src/entry2.js"
+    },
+    // 出口文件配置,支持多出口配置。
+    output:{
+        // 打包的路径地址
+        path:path.resolve(__dirname,"dist"),
+        // 打包的文件名称
+        filename:"[name].js"
+    },
+    // 模块：主要是解析CSS和图片转换压缩等功能。
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:["style-loader","css-loader"]
+            }
+        ]
+
+    },
+    // 插件，用户声场模板和各项功能
+    plugins:[
+
+    ],
+    // 配置webpack开发服务功能
+    devServer:{
+        // 设置基本目录结构
+        contentBase:path.resolve(__dirname,"dist"),
+        // 服务器的ip地址，可以使用ip也可以使用localhost
+        host:"localhost",
+        // 服务端压缩是否开启
+        compress:true,
+        // 配置服务端口号
+        port:3000
+
+    }
+}
