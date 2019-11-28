f86467dab46a5aac3311b28bc867bacdbd0d7a2e
renzhiwei <729079155@qq.com>
2019-11-28 19:42:41 +0800

    15

---
+++ 15给webpack增加babel支持/src/entry.js
@@ -0,0 +1,8 @@
+document.getElementById('title').innerHTML='Hello Webpack';
+import  css from './css/index.css';
+import  less from './css/black.less';
+import  scss from './css/blue.scss';
+
+
+console.log("成功了89999！");
+
