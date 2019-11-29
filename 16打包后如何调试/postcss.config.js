8d6e7c434a863b696239dc7f0dd70469d469ddb2
renzhiwei <729079155@qq.com>
2019-11-29 14:06:32 +0800

    暂停说明

---
+++ 16打包后如何调试/postcss.config.js
@@ -0,0 +1,6 @@
+// css3属性加入-webkit,-ms,-o,-moz这些前缀的配置
+module.exports = {
+    plugins: [
+        require("autoprefixer")
+    ]
+}
---
+++ 16打包后如何调试/src/entry.js
@@ -0,0 +1,19 @@
+document.getElementById('title').innerHTML='Hello Webpack';
+import  css from './css/index.css';
+import  less from './css/black.less';
+import  scss from './css/blue.scss';
+
+
+// console.log("成功了89999！");
+
+// const PI=3.14,
+//       k={
+//         a:1
+//       };
+// k.b=3;
+// console.log(PI,k);
+
+// function test(x, y='world') {
+//     console.log('默认值',x, y);
+// }
+// test();//undefined world
